2a9dda6c545c8da633d44e52ee59fbb4b5374b95
dattamlong <ndat9124@gmail.com>
2025-02-10 04:40:46 +0700

    init and add some components

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { BeforeEffectButton } from "@/components/ui/BeforeEffectButton";
+
+export default function Button() {
+  return <BeforeEffectButton hoverOnly={false}>Button</BeforeEffectButton>;
+}
