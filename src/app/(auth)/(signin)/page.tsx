3c0cd3df9c9328558b1477213e170c802a73be25
dattamlong <ndat9124@gmail.com>
2025-02-10 05:21:43 +0700

    add sign up page

---
+++ src/app/(auth)/(signin)/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import SignInViewPage from "@/components/sign-in-view";
+
+
+export const metadata: Metadata = {
+  title: "Authentication | Sign In",
+  description: "Sign In page for authentication."
+};
+
+export default async function Page() {
+  return <div><SignInViewPage /></div>;
+}
--- src/components/ui/Button.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { BeforeEffectButton } from "@/components/ui/BeforeEffectButton";
-
-export default function Button() {
-  return <BeforeEffectButton hoverOnly={false}>Button</BeforeEffectButton>;
-}
